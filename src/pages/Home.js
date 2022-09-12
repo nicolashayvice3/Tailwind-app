d441b5a9c79f3416ab7423b5aa8308b1025786cc
nicolashayvice3 <nicolashayvice@gmail.com>
2022-09-12 14:05:57 +1200

    home sec done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import { Fragment } from 'react'
 import { Popover, Transition } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import Logo from '../assets/pharmacylady.jpeg'
 
 const navigation = [
   { name: 'Featured', href: '#' },
@@ -35,11 +36,11 @@ export default function Example() {
                         <img
                             alt="Your Company"
                             className="h-8 w-auto sm:h-10"
-                            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                            src="https://tailwindui.com/img/logos/mark.svg?color=green&shade=600"
                         />
                         </a>
                         <div className="-mr-2 flex items-center md:hidden">
-                        <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                        <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-green-500">
                             <span className="sr-only">Open main menu</span>
                             <Bars3Icon className="h-6 w-6" aria-hidden="true" />
                         </Popover.Button>
@@ -52,7 +53,7 @@ export default function Example() {
                         {item.name}
                         </a>
                     ))}
-                    <a href="/login" className="font-medium text-indigo-600 hover:text-indigo-500">
+                    <a href="/login" className="font-medium text-green-600 hover:text-green-500">
                         Log in
                     </a>
                     </div>
@@ -77,12 +78,12 @@ export default function Example() {
                         <div>
                         <img
                             className="h-8 w-auto"
-                            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                            src="https://tailwindui.com/img/logos/mark.svg?color=green&shade=600"
                             alt=""
                         />
                         </div>
                         <div className="-mr-2">
-                        <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                        <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-green-500">
                             <span className="sr-only">Close main menu</span>
                             <XMarkIcon className="h-6 w-6" aria-hidden="true" />
                         </Popover.Button>
@@ -101,7 +102,7 @@ export default function Example() {
                     </div>
                     <a
                         href="/login"
-                        className="block w-full bg-gray-50 px-5 py-3 text-center font-medium text-indigo-600 hover:bg-gray-100"
+                        className="block w-full bg-gray-50 px-5 py-3 text-center font-medium text-green-600 hover:bg-gray-100"
                     >
                         Log in
                     </a>
@@ -114,7 +115,7 @@ export default function Example() {
                 <div className="sm:text-center lg:text-left">
                 <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
                     <span className="block xl:inline">Your favourite pharmacy</span>{' '}
-                    <span className="block text-indigo-600 xl:inline">all online</span>
+                    <span className="block text-green-600 xl:inline">all online</span>
                 </h1>
                 <p className="mt-3 text-base text-gray-500 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0">
                     We here at nz-online pharmacy are commited to providing you with the best possible shopping experience. Trusted by
@@ -124,7 +125,7 @@ export default function Example() {
                     <div className="rounded-md shadow">
                     <a
                         href="/login"
-                        className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 md:py-4 md:px-10 md:text-lg"
+                        className="flex w-full items-center justify-center rounded-md border border-transparent bg-green-600 px-8 py-3 text-base font-medium text-white hover:bg-green-700 md:py-4 md:px-10 md:text-lg"
                     >
                         Get started
                     </a>
@@ -132,7 +133,7 @@ export default function Example() {
                     <div className="mt-3 sm:mt-0 sm:ml-3">
                     <a
                         href="shopping"
-                        className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-100 px-8 py-3 text-base font-medium text-indigo-700 hover:bg-indigo-200 md:py-4 md:px-10 md:text-lg"
+                        className="flex w-full items-center justify-center rounded-md border border-transparent bg-green-100 px-8 py-3 text-base font-medium text-green-700 hover:bg-green-200 md:py-4 md:px-10 md:text-lg"
                     >
                         Shopping
                     </a>
@@ -145,7 +146,7 @@ export default function Example() {
         <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
             <img
             className="h-56 w-full object-cover hidden sm:flex sm:h-72 md:h-96 lg:h-full lg:w-full"
-            src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
+            src={Logo}
             alt=""
             />
         </div>
